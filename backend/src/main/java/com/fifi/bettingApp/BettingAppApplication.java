0f2846bf68184ba99e6ecd49cbbfaa15d959c88c
Filip Majewski <filip.majewski@student.pk.edu.pl>
2025-11-15 20:24:45 +0100

    test

--- backend/src/main/java/com/fifi/bettingApp/BettingAppApplication.java
+++ backend/src/main/java/com/fifi/bettingApp/BettingAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BettingAppApplication {
-
+    //test
 	public static void main(String[] args) {
 		SpringApplication.run(BettingAppApplication.class, args);
 	}
