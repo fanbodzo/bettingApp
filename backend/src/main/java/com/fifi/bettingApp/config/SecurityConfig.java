ddea118cd76d4b6e66624ce5ed0f22cd5607bc2f
Filip Majewski <filip.majewski@student.pk.edu.pl>
2025-12-08 21:30:42 +0100

    dodanie starznika do weryfijkacji tokenu

--- backend/src/main/java/com/fifi/bettingApp/config/SecurityConfig.java
+++ backend/src/main/java/com/fifi/bettingApp/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.fifi.bettingApp.config;
 
+import com.fifi.bettingApp.security.AuthTokenFilter;
 import com.fifi.bettingApp.security.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -8,9 +9,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,14 +22,23 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final UserDetailsServiceImpl userDetailsService;
+    //wstrzykniecie filtru tokenu
+    private final AuthTokenFilter authTokenFilter;
 
     //bean do definicji glownego lancucha filtrow bezpieczenstwa
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll());
+                //mowimy springowi zeby dzialal w trybie stateless daje topewnosc ze spring nie bedzie tworzyc
+                //sesji po stronie serwera i jej uzywal , cale uwierzytelnianie opiera sie na toknie jwt
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                //kazdy token ma dostep do podanych endpointow
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/users").permitAll()
+                .anyRequest().authenticated());
+        //wstawiamy walsnego straznika przed sprawdzniem loginu ihasla i wstawiam tam moj walsny authFilter
+        http.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
--- backend/src/main/java/com/fifi/bettingApp/security/JwtUtils.java
+++ backend/src/main/java/com/fifi/bettingApp/security/JwtUtils.java
@@ -1,12 +1,17 @@
 package com.fifi.bettingApp.security;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 
 import java.security.Key;
 import java.util.Date;
@@ -30,6 +35,8 @@ public class JwtUtils {
     @Value("${app.jwt.expiration-ms}")
     private int jwtExpirationMs;
 
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
     private Key key(){
         //bierzemy nasz sekret jwt i zamieniamy go na bezpieczy hash
         //kryptograficzny klucz uzywany dopodpisywania
@@ -51,6 +58,33 @@ public class JwtUtils {
                 .setIssuedAt(new Date()).setExpiration(new Date((new Date()).getTime()+jwtExpirationMs))
                 //skladamy w calosc podpisujemy kluczem i mozemy wysylac to jakobezpieczny string
                 .signWith(key(), SignatureAlgorithm.HS256).compact();
+    }
+
+    //metoda zeby otrzymac username z tokenu
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key()).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+    //metoda do waalidacji tokenu
+    //nie jest to wjednej metodzeie zeby zachowac zasade S z solid
+    //S - single responsibility
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key()).build().parse(authToken);
+            return true;
+        } catch (SignatureException e) {
+            logger.error("Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+
+        return false;
     }
 
 }
