48cc99e8f64d2109c22800164290f7551c5f12c8
Filip Majewski <filip.majewski@student.pk.edu.pl>
2025-12-18 23:50:12 +0100

    contorller do stawania betow

---
+++ backend/src/main/java/com/fifi/bettingApp/controller/BetController.java
@@ -0,0 +1,22 @@
+package com.fifi.bettingApp.controller;
+
+import com.fifi.bettingApp.dto.PlaceBetRequestDto;
+import com.fifi.bettingApp.entity.Bet;
+import com.fifi.bettingApp.service.BetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/bets")
+@RequiredArgsConstructor
+public class BetController {
+    private final BetService betService;
+
+    @PostMapping("/{userId}")
+    public ResponseEntity<Void> placeBet(@PathVariable Long userId , @RequestBody PlaceBetRequestDto requestDto){
+        betService.placeBet(userId, requestDto.getStake());
+
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ backend/src/main/java/com/fifi/bettingApp/dto/PlaceBetRequestDto.java
@@ -0,0 +1,8 @@
+package com.fifi.bettingApp.dto;
+
+import lombok.Data;
+
+@Data
+public class PlaceBetRequestDto {
+    private Double stake;
+}
